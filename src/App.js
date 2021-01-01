24185aee38882f1461aef5ffc248c48b2e4ded5a
Renan Bona <rcbp3094@gmail.com>
2021-01-01 11:03:26 -0300

    First component React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import Header from './components/Header';
+import image from './assets/crash.jpg'
+import api from './services/Api';
 
 function App() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect( () => {
+    api.get('/things').then( response => {
+      setProjects(response.data.data);
+    });
+  },[]);
+
+  async function handleAddProject() {
+    // setProjects([...projects, Date.now()]);
+
+    const response = await api.post("/things", {
+      "thing": {
+        "name": `Project ${Date.now()}`
+      }
+    });
+
+    const project = response.data.data;
+
+    setProjects([...projects, project])
+  }
+
+  async function handleDeleteProject(project) {
+    await api.delete(`/things/${project.attributes.slug}`).then(
+      () => {
+        api.get('/things').then( response => {
+          setProjects(response.data.data);
+        });
+      }
+    );
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>This time I'll master React!!!</h1>
+
+      <Header title="Goku" />
+      <img src={image}/>
+
+      <ul>
+        {projects.map( project =>
+          <div key={project.id} className="list-item">
+            <li>{project.attributes.name}</li>
+            <button onClick={ () => handleDeleteProject(project)} className="delete-button">Delete</button>
+          </div>
+        )}
+      </ul>
+
+      <button type="button" onClick={handleAddProject}>Add Project</button>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import { render } from '@testing-library/react';
+import React, { Children } from 'react';
+
+export default function Header({ title }) {
+  return (
+    <header>
+      <h1>{title}</h1>
+    </header>
+  );
+}
